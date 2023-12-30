6c0dd794b5712d0fa100cc018863ef131f8216eb
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2023-12-30 23:01:49 +0000

    Memory Allocation

---
+++ 01_Basics/06_StackHeap.js
@@ -0,0 +1,29 @@
+//Stack Heap Memory
+
+// TYPES :
+// Stack  [Primitive]
+// Heap  [Non Primitive]
+
+//                              Stack Memory Work Flow =>
+
+let myYoutubename = "Aniketrautyt"
+
+let anothername = "Myyoutube"
+anothername = "Chai Aur Code"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+//                              Heap Memory Allocation
+
+let userOne ={
+    email: "user@google.com"
+    
+}
+
+let userTwo = userOne
+
+userTwo.email = "Aniketr111.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
