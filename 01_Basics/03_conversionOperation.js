36c583e1781799a2180884b62a2ff94c60594137
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2023-12-30 20:04:13 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// let str1 = "Hello"
+// let str2 = " Aniket"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((2 + 2) * 5 % 3);
+
+//TRICKY CONVERSION
+console.log(+true);             //1
+console.log(+"");              //0
+
+
+
+
