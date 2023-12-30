217ee4adb4ecb032779ab80ac3d0c5304b930a3a
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2023-12-30 22:24:29 +0000

    Summary Interview

---
+++ 01_Basics/05_Brushupdatatypes.js
@@ -0,0 +1,23 @@
+// # PRIMITIVE DATATYPES 
+//  7 TYPES : string, Number, Boolean, Null, Undefined, Symbol, BigInt.
+
+// Javascript Is A Dynamically Typed Language
+
+// #REFERENCE TYPE [ Non Primitive datatypes]
+
+// Array, Objects, Functions
+
+const heros =["shaktiman", "Sonpari", "Nattukaka"];
+
+let myObj = {
+    name: "Aniket",
+    age: 24
+}
+
+const myFunction = function() {
+    console.log("Hello Word")
+    
+}
+
+console.log(typeof myFunction);
+
