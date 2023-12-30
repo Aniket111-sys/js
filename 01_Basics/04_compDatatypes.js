f48f959dccb7364216d94074bb478739e5d20697
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2023-12-30 21:56:34 +0000

    datatypes comparison

---
+++ 01_Basics/04_compDatatypes.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//STRING
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//NULL COMP   EQUALITY CHECK
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//STRICT CHECK  ==== CHECK THE VALUES AS WELL AS DATATYPES     ** DIFFERENT DATATYPES
+
+// ==== its checks values as well as datatype
+console.log("2" === 2 );
+
+//  ==   it convert any datatypes into the form of number
+console.log("2" > 1);
+
+
