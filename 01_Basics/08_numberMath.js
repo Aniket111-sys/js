502bc9cd6fa87100009d1569276597d7eacd511d
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-02 22:32:56 +0000

    Math And Num

---
+++ 01_Basics/08_numberMath.js
@@ -0,0 +1,35 @@
+                                                      // Number 
+// const score = 200
+// console.log(score);
+
+// const balance = new Number(150)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.3332
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+                                                    //  Math
+
+console.log(Math.abs(-4));                                                   
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.7));
+console.log(Math.floor(4.7));
+console.log(Math.min(4, 8, 3, 9));
+console.log(Math.max(4, 8, 3, 9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random()* (max - min + 1)) + min );
+
