8545379abf8f8d12f240011af0b15420f315e846
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-03 21:19:16 +0000

    Array

---
+++ 01_Basics/10_Array.js
@@ -0,0 +1,40 @@
+///                                              ARRAY
+
+const myArray = [0, 2, 5, 8, 9, 7]
+//console.log(myArray);
+
+//                                                     ARRAY Methods
+//                                           Push (Add value)- Pop (Remove last value) - 
+
+//myArray.push(6, 1)
+//console.log(myArray);
+
+//                                            shift(Remove First Value) - Unshift(Add Value At Firast Position)
+
+//myArray.unshift(3)
+// myArray.shift()
+// console.log(myArray);
+
+
+//                                             .Join  (it Convert Array into string )
+
+
+// const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//                                             Slice [not manuplate the Array] And Splice [Manuplate the Araay ]
+
+ console.log("A", myArray);
+
+ const myn1 = myArray.slice(2, 5)
+
+ console.log(myn1);
+ console.log("B", myArray);
+
+ const myn2 = myArray.splice(2, 5)
+ console.log(myn2);
+ console.log("C", myArray);
+
