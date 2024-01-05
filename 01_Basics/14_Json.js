bf2aca777a31345ccbdcdb3c3342d0a5def2088e
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-05 00:57:53 +0000

    Json Basics

---
+++ 01_Basics/14_Json.js
@@ -0,0 +1,33 @@
+//console.log("ovi");
+
+
+//                     **************Object Destructuring***********
+
+const course = {
+    courseName: "Js",
+    price: 999,
+    Instructor: "Akshay"
+}
+
+const {Instructor:name, price} = course
+//console.log(Instructor);
+console.log(name, price);
+
+//                       ********Json-API***********
+//     Syntax of Json
+
+// {
+//     "name": "Ovi"
+//     "courseName": "Js"
+//     "Price":  "1604"
+// }
+
+                           //Json Structure In form of Array
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
+
+
