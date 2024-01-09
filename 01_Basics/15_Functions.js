3a64faac3da3e5226d2a1f52113449b4d5736735
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-09 04:17:22 +0000

    Functions p1

---
+++ 01_Basics/15_Functions.js
@@ -0,0 +1,40 @@
+//console.log("aniket");
+
+const num1 = [233, 433, 400]
+
+function calculateCartPrice(...num1){
+
+// OR
+//function calculateCartPrice(val1, val2, ...num1){
+
+    return num1
+}
+//console.log(calculateCartPrice(...num1));
+
+
+
+const user ={
+    username: "Aniket",
+    price: 200
+}
+function handleObject(anyobject){
+   // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+
+//OR
+
+handleObject({
+    username: "aniket",
+    price: 400
+})
+
+const myNewarray = [200, 330, 555, 434, 558]
+
+function returnseconValue(getarray){
+return getarray[3]
+}
+//console.log(returnseconValue(myNewarray));
+//OR
+console.log([200, 433, 555, 654]);
