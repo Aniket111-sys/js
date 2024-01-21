50a9c8765e0d1f742150a0912282f9dd4a6a28b5
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-21 22:13:47 +0000

    p1

---
+++ 03_Practice/one.js
@@ -0,0 +1,27 @@
+
+//                     Question 1
+
+// var x = 10;
+// (function (){
+
+//console.log(x);
+// if(true){
+//     var x = 20;
+  //  console.log(x);
+// }
+// })
+//console.log(x);
+
+//                    Question 2
+
+
+let y = 10;
+
+(function () {
+console.log(y);
+if(true) {
+    let x = 20;
+    console.log(y);
+}
+})
+console.log(y);
