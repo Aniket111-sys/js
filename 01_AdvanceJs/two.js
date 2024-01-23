4250470b7208af5d5c587bdbbda5f1049feab217
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-23 20:05:54 +0000

    Switch Case

---
+++ 01_AdvanceJs/two.js
@@ -0,0 +1,41 @@
+//          Switch case Stmt
+//                                Syntax
+/*switch (Kye){
+case value:
+
+break;
+
+default:
+
+break;
+}
+*/
+
+
+
+//                                 code
+
+const month = 1;
+
+switch (month) {
+
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case match");
+        break;
+}
+
+
+
