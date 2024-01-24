8381f84589af8858e3770fc9776b3b5e06d6f08e
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-24 11:20:10 +0000

    While loop

---
+++ 01_AdvanceJs/While.js
@@ -0,0 +1,28 @@
+// console.log('ovi');
+
+//                             While loop
+
+let index = 0
+while (index <= 30) {
+    console.log(`Value of index is ${index}`);
+    index = index + 1 * 3
+}
+
+
+
+let Myarr = ['Spiderman', 'Superman','Hanuman']
+let arr = 0
+
+while (arr < Myarr.length) {
+    console.log(`Value is ${Myarr[arr]}`);
+    arr = arr + 1
+}
+
+
+
+let Score = 1
+
+do {
+    console.log();
+Score = Score++
+} while (Score <= 20);
