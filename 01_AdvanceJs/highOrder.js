6fd3d6b56c30548ad79673726a953f4daf6bcd80
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-01-24 20:34:33 +0000

    map , for of

---
+++ 01_AdvanceJs/highOrder.js
@@ -0,0 +1,37 @@
+//console.log('ovi');
+
+//                          High Order         1] for of     2] 
+
+//const arr = [1, 2, 3, 4, 5, 6]
+
+//for (const num of arr) {
+   /// console.log(num);
+//}
+
+
+//const greeting = "Hello Word"
+
+//for (const omg of greeting) {
+  //  console.log(`Each char is ${omg}`);
+//}
+
+// Maps
+
+
+let map = new Map()
+
+map.set('IN', "INDIA")
+map.set('USA', "United States")
+map.set('Is', "Israel")
+
+//console.log(map);
+
+
+
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-' , value);
+// }
+
+
+
