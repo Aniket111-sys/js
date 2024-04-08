88c377028103ec7a60aa2eab30c5d0999f5e18da
Aniket111-sys <135662216+Aniket111-sys@users.noreply.github.com>
2024-04-08 01:24:18 +0000

    ConditionalStmt

---
+++ 03_Practice/ifElse.js
@@ -0,0 +1,25 @@
+//                                    ##ifStatement
+
+
+let num = 23;
+ 
+if (num % 2 === 0) {
+    console.log("Given number is even number.");
+}
+ 
+if (num % 2 !== 0) {
+    console.log("Given number is odd number.");
+};
+
+//                        youCanVoteOrNot
+
+var age = 15;
+var votingAge = 18;
+
+if(age > votingAge) {
+  console.log("Old enough to vote!");
+}
+else {
+  console.log("Not old enough to vote.");
+}
+
